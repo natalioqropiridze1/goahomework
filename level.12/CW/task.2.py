9a88f162c0e4aec0cf0b09296fd544ceaee5c7a1
natalioqropiridze1 <natalia.okropiridze1@gmail.com>
2025-03-23 22:45:34 +0400

    commit

---
+++ level.12/CW/task.2.py
@@ -0,0 +1,16 @@
+"""2) სამივე ლოგიკურ ოპერატორზე: not, or, and: ჩამოწერეთ ყველა შესაძლო ვარიანტი და დაბეჭდეთ შედეგი გვერდზე კომენტარის საშვალებით მიწუერეთ შედეგი"""#
+# not 
+print(not True)   #False
+print(not False)  #True
+
+# or 
+print(True or True)   # True
+print(True or False)  # True
+print(False or True)  # True
+print(False or False) # False
+
+# and 
+print(True and True)   #True
+print(True and False)  #False
+print(False and True)  #False
+print(False and False) #False
---
+++ level.12/CW/task1.py
@@ -0,0 +1,10 @@
+
+a = 10
+b = 5
+
+# შედარების ოპერაციები
+print(a == b)  #True
+print(a > b)   # True
+print(a < b)   #False
+print(a >= b)  #True
+print(a <= b)  # False
